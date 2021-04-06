94461c026b4a6416179b50df98d2b35c1478ac7d
megmcgowan <megmcgowan@deloitte.ie>
2021-04-06 13:28:04 +0100

    Replacing missing values

---
+++ Missing Values.py
@@ -0,0 +1,36 @@
+import pandas as pd
+
+train_data = pd.read_csv("aug_train.csv")
+print(train_data)
+
+test_data = pd.read_csv("aug_test.csv")
+print(test_data)
+
+#Train dataframe analysis
+print(train_data.head())
+print(train_data.info())
+
+#Test dataframe analysis
+print(test_data.head())
+print(test_data.info())
+
+#Merge dataframes using concat
+hr_analysis_concat=pd.concat([test_data,train_data])
+print(hr_analysis_concat.info())
+
+#Identify the number of missing values in each column
+missing_values=hr_analysis_concat.isnull().sum()
+print(missing_values)
+
+#Replaceing missing values for gender and enrolled university
+hr_analysis_concat['gender'] = hr_analysis_concat['gender'].fillna('Unknown')
+hr_analysis_concat['enrolled_university'] = hr_analysis_concat['enrolled_university'].fillna('No Enrollment ')
+
+#Replaceing missing values for all other columns
+hr_analysis=hr_analysis_concat.fillna(0, inplace=True)
+
+#Confirm missing values have been populated
+missing_values=hr_analysis_concat.isnull().sum()
+print(missing_values)
+
+print(hr_analysis_concat.info())
