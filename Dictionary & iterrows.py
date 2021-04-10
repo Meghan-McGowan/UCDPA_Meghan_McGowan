8d830ca1d113f05bf70a83560da2d473ee974b64
megmcgowan <megmcgowan@deloitte.ie>
2021-04-10 17:23:38 +0100

    functions

--- Dictionary & iterrows.py
+++ Dictionary & iterrows.py
@@ -24,7 +24,8 @@ print(missing_values)
 
 #Replaceing missing values for gender and enrolled university
 hr_analysis_concat['gender'] = hr_analysis_concat['gender'].fillna('Other')
-hr_analysis_concat['enrolled_university'] = hr_analysis_concat['enrolled_university'].fillna('No Enrollment ')
+hr_analysis_concat['enrolled_university'] = hr_analysis_concat['enrolled_university'].fillna('no_enrollment')
+hr_analysis_concat['major_discipline'] = hr_analysis_concat['major_discipline'].fillna('NA')
 
 #Replaceing missing values for all other columns using numpy
 hr_analysis=hr_analysis_concat.fillna(0, inplace=True)
@@ -34,6 +35,14 @@ missing_values=hr_analysis_concat.isnull().sum()
 print(missing_values)
 
 print(hr_analysis_concat.info())
+
+#Replacing values where >20
+hr_analysis_concat['experience']. replace(['>20', '<1'],['21', '1'], inplace=True)
+print(hr_analysis_concat['experience']. unique())
+
+#Replacing last new job values
+hr_analysis_concat['last_new_job']. replace(['>4', 'never'],['5', '0'], inplace=True)
+print(hr_analysis_concat['last_new_job']. unique())
 
 #Create a Dictionary of Lists
 hr_dictionary = {'gender': ['male', 'female', 'other'],
--- Functions.py
+++ Functions.py
@@ -7,13 +7,27 @@ female_education =[female_education1,female_education2,female_education3]
 
 male_education1 = {'education_level': 'Masters', 'major-disciplne': 'STEM'}
 male_education2 = {'education_level': 'Graduate', 'major-disciplne': 'Business Degree'}
-male_education3 = {'education_level': 'Graduate', 'major-disciplne': 'STEM'}
+male_education3 = {'education_level': 'Masters', 'major-disciplne': 'STEM'}
 male_education =[male_education1,male_education2,male_education3]
 
-# function for selecting a loop key
-def loop_key():
-    for i in female_education:
-        print(i['education_level'])
-loop_key(male_education)
+#Iterate through the rows using a loop
+for i in female_education:
+    print(i)
+
+#Iterate through the rows by the education_level key
+for i in female_education:
+    print(i['education_level'])
+
+#Loop and condition
+selected_education_level = {}
+education_level_lookup = "Masters"
+for i in female_education:
+        if i['education_level'] == education_level_lookup:
+            selected_education_level = i
+
+print(selected_education_level)
+
+
+
 
 
